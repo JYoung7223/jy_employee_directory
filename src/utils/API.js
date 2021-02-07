ead50c6f4da09f66d934878fa9a00286b708c5fc
[Justin Young] <[jyoung7223@yahoo.com]>
2021-02-06 22:07:53 -0700

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the random user API
-function getUsers(numUsers){
-  return axios.get(`https://cors-anywhere.herokuapp.com/http://randomuser.me/api/?results=${numUsers}`);
-}
-export default {
-  getUsers
-};

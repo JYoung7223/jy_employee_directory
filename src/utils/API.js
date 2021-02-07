ca4b9d0524ca3ff38add1d0520751b08ee635351
[Justin Young] <[jyoung7223@yahoo.com]>
2021-02-06 22:17:50 -0700

    Changing to https

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the random user API
 function getUsers(numUsers){
   // if localhost try using: `https://cors-anywhere.herokuapp.com/http://randomuser.me/api/?results=${numUsers}`
-  return axios.get(`http://randomuser.me/api/?results=${numUsers}`);
+  return axios.get(`https://randomuser.me/api/?results=${numUsers}`);
 }
 export default {
   getUsers

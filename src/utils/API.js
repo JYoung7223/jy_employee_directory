b2c78650ccb04304fe47bfe34fe61a776513b05f
[Justin Young] <[jyoung7223@yahoo.com]>
2021-02-06 22:13:18 -0700

    Deploying to Github Pages

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the random user API
 function getUsers(numUsers){
-  return axios.get(`https://cors-anywhere.herokuapp.com/http://randomuser.me/api/?results=${numUsers}`);
+  // if localhost try using: `https://cors-anywhere.herokuapp.com/http://randomuser.me/api/?results=${numUsers}`
+  return axios.get(`http://randomuser.me/api/?results=${numUsers}`);
 }
 export default {
   getUsers
